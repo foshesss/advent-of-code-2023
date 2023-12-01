ab96f66e307ff72071e4d553c15970d22b70cc0c
foshesss <foshesss@gmail.com>
2023-12-01 00:31:19 -0500

    took a lot longer than most years

--- src/day1.js
+++ src/day1.js
@@ -5,4 +5,68 @@ const content = fs.readFileSync(
     { encoding: 'utf8', flag: 'r' }
 )
 
-console.log(content.split('\n'))
+const lines = content.split('\n')
+
+let p1total = 0
+
+for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
+
+    let first, last
+
+    for (let j = 0; j < line.length; j++) {
+        let n = parseInt(line[j])
+        if (isNaN(n)) continue
+
+        if (first == undefined) {
+            first = n
+        }
+        last = n
+    }
+
+    p1total += parseInt(first.toString() + last.toString())
+}
+
+console.log("Part 1: ", p1total)
+
+
+const look_for = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    1, 2, 3, 4, 5, 6, 7, 8, 9
+]
+
+let p2total = 0
+
+for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
+
+    let first, last
+    let firstIndex = Number.MAX_VALUE // set to really high number, guaranteeing first pos
+    let lastIndex = -1 // set to really low number, guaranteeing last pos
+
+    look_for.forEach((value, index) => {
+        const lowest_index = line.indexOf(value)
+        const highest_index = line.lastIndexOf(value)
+
+        // convert strings to numbers
+        if (index < 9) {
+            value = index + 1
+        }
+
+        // check for lowest position
+        if (lowest_index != -1 && firstIndex > lowest_index) {
+            first = value
+            firstIndex = lowest_index
+        }
+
+        // check for highest position
+        if (highest_index != -1 && lastIndex < highest_index) {
+            last = value
+            lastIndex = highest_index
+        }
+    })
+
+    p2total += parseInt(first.toString() + last.toString())
+}
+
+console.log("Part 2: ", p2total)
